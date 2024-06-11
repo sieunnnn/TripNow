96391bc9fcf90ed9c29a0478abf4f146428f7532
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-11 21:44:41 +0900

    Feat: axios instance 생성

---
+++ src/api/AxiosInstance.ts
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://api.example.com';
+
+const axiosInstance = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('Authorization');
+        if (accessToken) {
+            config.headers['Authorization'] = `${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && error.response.data?.errorCode === 'TOKEN_01' && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const refreshToken = localStorage.getItem('RefreshToken');
+                if (!refreshToken) {
+                    throw new Error('No refresh token available');
+                }
+
+                const response = await axios.post(`${API_BASE_URL}/api/v1/auth/token/refresh`, {
+                    token: refreshToken,
+                });
+
+                const { accessToken } = response.data;
+                localStorage.setItem('Authorization', accessToken);
+                axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+                originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+
+                return axiosInstance(originalRequest);
+            } catch (e) {
+                console.error('Token refresh failed', e);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
