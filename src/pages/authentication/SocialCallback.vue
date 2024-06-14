3d36339be402db0d0d60e08a38570e2b482f87ea
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-15 01:57:07 +0900

    Feat: 로그인 / 소셜 로그인 기능 초안 작성

---
+++ src/pages/authentication/SocialCallback.vue
@@ -0,0 +1,30 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+onMounted(() => {
+  const userId = route.query.userId as string;
+  const nickname = route.query.nickname as string;
+  const userTag = route.query.userTag as string;
+  const profileImgUrl = route.query.profileImgUrl as string;
+
+
+
+  localStorage.setItem('userId', userId);
+  localStorage.setItem('nickname', nickname);
+  localStorage.setItem('userTag', userTag);
+  localStorage.setItem('profileImgUrl', profileImgUrl);
+
+  router.push('/');
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
