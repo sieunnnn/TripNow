1940ceb172f22f7b80fb38d8f060dda02d27c8f8
sieunnnn <wldsmtldsm65@gmail.com>
2024-08-21 14:48:09 +0900

    Fix: ngrok 설정

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://5161-1-215-153-253.ngrok-free.app',
     timeout: 1000,
 });
 
